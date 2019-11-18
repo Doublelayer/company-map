dd9d4277aeca12fe9cfc4189328a81e08202c846
Doublelayer <florin.hamann@web.de>
2019-11-18 19:58:38 +0100

    map now auto fitting bounds after loading company positions

--- client/src/components/parcels/map/companyMap.js
+++ client/src/components/parcels/map/companyMap.js
@@ -1,36 +1,57 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import { bindActionCreators } from "redux";
-import { markerConfig } from "./MapMarkerConfig";
-import { Map, TileLayer, Marker, Popup } from "react-leaflet";
-import MarkerClusterGroup from "react-leaflet-markercluster";
+import React, { createRef, Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { markerConfig } from './MapMarkerConfig';
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
+import MarkerClusterGroup from 'react-leaflet-markercluster';
 
-import { toogleGroupedMarkers } from "../../../store/actions/showHide";
+import { toogleGroupedMarkers } from '../../../store/actions/showHide';
 
-import "react-leaflet-markercluster/dist/styles.min.css";
+import 'react-leaflet-markercluster/dist/styles.min.css';
 
-import "./companyMap.css";
+import './companyMap.css';
 
 class CompanyMap extends Component {
+  constructor(props) {
+    super(props);
+    this.mapRef = createRef();
+    this.groupRef = createRef();
+  }
+
+  componentDidUpdate(prevProps) {
+    this.props.markerData !== prevProps.markerData && this.mapFitBounds();
+  }
+
+  mapFitBounds() {
+    const { mapRef, markerData } = this.props;
+    if (this.props.markerData.length > 0) {
+      const map = mapRef.current.leafletElement;
+      const res = markerData.map(data => ({ lat: data.latitude, lng: data.longitude }));
+      map.animate = true;
+      map.fitBounds(res);
+    }
+  }
+
   render() {
     const { userPosition, haveUserLoacation, markerData, toogleGroupedMarkers, showGroupedMarkers } = this.props;
-    const ShowGroupedMarkers = showGroupedMarkers ? MarkerClusterGroup : "div";
+    const ShowGroupedMarkers = showGroupedMarkers ? MarkerClusterGroup : 'div';
     const groupedControlImg = showGroupedMarkers
-      ? "https://www.pinclipart.com/picdir/middle/46-460577_maps-vector-graphic-google-maps-icon-android-clipart.png"
-      : "https://i.stack.imgur.com/g1erW.png";
+      ? 'https://www.pinclipart.com/picdir/middle/46-460577_maps-vector-graphic-google-maps-icon-android-clipart.png'
+      : 'https://i.stack.imgur.com/g1erW.png';
+
     return (
       <div className="map-container">
-        <Map className="map" center={userPosition} zoom={12}>
+        <Map className="map" center={userPosition} zoom={12} ref={this.mapRef} render={console.log('asdfa')}>
           <TileLayer
             attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
 
-          {haveUserLoacation ? <Marker position={userPosition} icon={markerConfig.User}></Marker> : ""}
+          {haveUserLoacation ? <Marker position={userPosition} icon={markerConfig.User}></Marker> : ''}
 
-          <ShowGroupedMarkers>
+          <ShowGroupedMarkers ref={this.groupRef}>
             {markerData.map(data => (
-              <Marker key={data._id} position={[data.latitude, data.longitude]} icon={markerConfig[data.sector.replace(/\s/g, "_")]}>
+              <Marker key={data._id} position={[data.latitude, data.longitude]} icon={markerConfig[data.sector.replace(/\s/g, '_')]}>
                 <Popup>
                   <div>
                     <a href={data.homepage} target="_blank" rel="noopener noreferrer">
@@ -68,7 +89,4 @@ const mapDispatchToProps = dispatch =>
     dispatch
   );
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(CompanyMap);
+export default connect(mapStateToProps, mapDispatchToProps)(CompanyMap);
